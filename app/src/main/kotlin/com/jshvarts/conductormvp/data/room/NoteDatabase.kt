24b348648cbdaf581746c04e65448f241698961b
James Shvarts <jshvarts@gmail.com>
2017-11-21 09:34:45 -0500

    remove unused DateConverter

--- app/src/main/kotlin/com/jshvarts/conductormvp/data/room/NoteDatabase.kt
+++ app/src/main/kotlin/com/jshvarts/conductormvp/data/room/NoteDatabase.kt
@@ -2,10 +2,8 @@ package com.jshvarts.conductormvp.data.room
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import android.arch.persistence.room.TypeConverters
 
 @Database(entities = arrayOf(NoteEntity::class), version = 1, exportSchema = false)
-@TypeConverters(DateConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): RoomNoteDao
 }
